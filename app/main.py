91eb1e3a189f928a3a4fb860568c5803dde96361
unknown <s.denis1125@gmail.com>
2023-12-01 21:44:46 -0800

    added db connection and table

--- app/main.py
+++ app/main.py
@@ -1,8 +1,45 @@
-from fastapi import FastAPI
+from datetime import date
+from typing import Optional
+from fastapi import Depends, FastAPI, Query
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
 
+class SRental(BaseModel):
+    adress: str
+    name: str
+    stars: int
+
+
+class RentalsSearchArgs:
+    def __init__(
+        self,
+        location: str,
+        date_from: date,
+        date_to: date,
+        stars: Optional[int] = Query(None, ge=1, le=5)
+    ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.stars = stars
+
+
 @app.get('/rentals')
-def get_rentals():
-    return 'Rentals in your area:'
+def get_rentals(
+        search_args: RentalsSearchArgs = Depends()
+):
+    return search_args
+
+
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+@app.post('/bookings')
+def add_booking(booking: SBooking):
+    pass
